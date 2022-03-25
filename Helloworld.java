aaf6a9005177b4e3245cd85e2e22adfca1176811
EdenMengesha52844 <100151122+Edenmengesha@users.noreply.github.com>
2022-03-25 16:51:02 +0100

    Create Helloworld.java

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, World. My name is Eden Mengesha and My student id is 52844;
+    }
+}
